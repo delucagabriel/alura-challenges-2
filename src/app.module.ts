18c952d25bcf908273e5c12b607aa4b9632bb465
Gabriel Mendes de Luca <gml3x7@gmail.com>
2021-08-14 20:41:34 -0300

    feat: implement video resource
    implement the resource videos with your CRUD, DTO and Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VideosModule } from './videos/videos.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: 3306,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        synchronize: true,
+        autoLoadEntities: true,
+        keepConnectionAlive: true,
+      }),
+    }),
+    VideosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
